5689daeba38881728a78b181165912a9a18c9e17
Assaf Toledo <assaf.toledo@ibm.com>
2020-09-28 16:01:54 +0300

    updated tests

--- tests/test_sib.py
+++ tests/test_sib.py
@@ -3,6 +3,7 @@ import pytest
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.datasets import fetch_20newsgroups
+from sklearn import metrics
 from scipy.sparse import csr_matrix
 from sib import SIB
 
@@ -10,25 +11,40 @@ from sib import SIB
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
 setups = {
-    'baseline':      {'n_cls': 20, 'n_jobs': 1, 'n_init': 1, 'max_iter': 15,
-                      'tol': 0.02, 'uniform': True, 'sparse': True, 'opt': 'B'},
-    'n_init':        {'n_cls': 20, 'n_jobs': 1, 'n_init': 8, 'max_iter': 15,
-                      'tol': 0.02, 'uniform': True, 'sparse': True, 'opt': 'C'},
-    'n_jobs':        {'n_cls': 20, 'n_jobs': -1, 'n_init': 8, 'max_iter': 15,
-                      'tol': 0.02, 'uniform': True, 'sparse': True, 'opt': 'C'},
-    'max_iter':      {'n_cls': 20, 'n_jobs': 1, 'n_init': 1, 'max_iter': 10,
-                      'tol': 0.02, 'uniform': True, 'sparse': True, 'opt': 'C'},
-    'tol':           {'n_cls': 20, 'n_jobs': 1, 'n_init': 1, 'max_iter': 15,
+    'baseline':      {'n_cls': 20, 'n_jobs': 1, 'n_init': 1, 'max_iter': 8,
                       'tol': 0.05, 'uniform': True, 'sparse': True, 'opt': 'B'},
-    'uniform_prior': {'n_cls': 20, 'n_jobs': 1, 'n_init': 1, 'max_iter': 15,
-                      'tol': 0.02, 'uniform': False, 'sparse': True, 'opt': 'C'},
-    'dense':          {'n_cls': 20, 'n_jobs': 1, 'n_init': 1, 'max_iter': 15,
-                       'tol': 0.02, 'uniform': True, 'sparse': False, 'opt': 'B'},
+    'n_init':        {'n_cls': 20, 'n_jobs': 1, 'n_init': 8, 'max_iter': 8,
+                      'tol': 0.05, 'uniform': True, 'sparse': True, 'opt': 'C'},
+    'n_jobs':        {'n_cls': 20, 'n_jobs': -1, 'n_init': 8, 'max_iter': 8,
+                      'tol': 0.05, 'uniform': True, 'sparse': True, 'opt': 'C'},
+    'max_iter':      {'n_cls': 20, 'n_jobs': 1, 'n_init': 1, 'max_iter': 3,
+                      'tol': 0.02, 'uniform': True, 'sparse': True, 'opt': 'C'},
+    'tol':           {'n_cls': 20, 'n_jobs': 1, 'n_init': 1, 'max_iter': 8,
+                      'tol': 0.10, 'uniform': True, 'sparse': True, 'opt': 'B'},
+    'uniform_prior': {'n_cls': 20, 'n_jobs': 1, 'n_init': 1, 'max_iter': 8,
+                      'tol': 0.05, 'uniform': False, 'sparse': True, 'opt': 'C'},
+    'float':         {'n_cls': 20, 'n_jobs': 1, 'n_init': 1, 'max_iter': 8,
+                      'tol': 0.05, 'uniform': False, 'sparse': True, 'opt': 'C', 'type': 'float'},
+    'dense':          {'n_cls': 20, 'n_jobs': 1, 'n_init': 1, 'max_iter': 8,
+                       'tol': 0.05, 'uniform': True, 'sparse': False, 'opt': 'B'},
 }
 
-equal_refs = [['baseline', 'dense'], ['n_init', 'n_jobs']]
+equal_refs = [['baseline', 'dense'], ['n_init', 'n_jobs'], ['uniform_prior', 'float']]
+
+ami_values = {
+    'baseline': 0.479308645259772,
+    'n_init': 0.4839664852243509,           # higher than baseline because uses more inits sequentially
+    'n_jobs': 0.4839664852243509,           # should be identical to n_init
+    'max_iter': 0.4054343178732652,         # less than baseline because less iterations
+    'tol': 0.42316606323860384,             # less than baseline because higher tol
+    'uniform_prior': 0.4833358896883293,    # normally higher than baseline
+    'float': 0.4833358896883293,            # should be identical to uniform_prior
+    'dense': 0.479308645259772              # should be identical to baseline
+}
 
 vectors_path = os.path.join(base_dir, 'resources', 'vectors')
+
+gold_labels_path = os.path.join(base_dir, 'resources', 'gold_labels')
 
 random_state = 527802
 
@@ -58,6 +74,11 @@ def is_sparse(setup_name):
     return setup['sparse']
 
 
+def is_float(setup_name):
+    setup = setups[setup_name]
+    return 'type' in setup and setup['type'] == 'float'
+
+
 def get_path(setup_name):
     return os.path.join(base_dir, 'resources', setup_name + '_ref')
 
@@ -84,6 +105,8 @@ def generate(setup_name, vectors):
         print("Generating reference for: %s" % setup_name)
         if not is_sparse(setup_name):
             vectors = vectors.toarray()
+        if is_float(setup_name):
+            vectors = vectors.astype(np.float)
         sib = create(setup_name, optimizer='B')
         sib.fit(vectors)
         save(setup_name, sib.labels_, sib.costs_, sib.score_)
@@ -107,7 +130,7 @@ def vectorize_20ng():
     dataset = fetch_20newsgroups(subset='train', categories=None,
                                  shuffle=True, random_state=256)
     vectorizer = CountVectorizer(max_features=max_features)
-    return vectorizer.fit_transform(dataset.data)
+    return vectorizer.fit_transform(dataset.data), dataset.target
 
 
 def load_vectors():
@@ -121,7 +144,7 @@ def supply_vectors():
 
 
 def generate_references():
-    vectors = vectorize_20ng()
+    vectors, gold_labels = vectorize_20ng()
     for setup_name in setups.keys():
         generate(setup_name, vectors)
     if not os.path.exists(vectors_path):
@@ -129,6 +152,13 @@ def generate_references():
         print("Saving vectors to: %s" % vectors_path)
         np.savez_compressed(os.path.join(vectors_path, "data.npz"),
                             data=vectors.data, indptr=vectors.indptr, indices=vectors.indices)
+    else:
+        print("Vectors dump is already generated")
+
+    if not os.path.exists(gold_labels_path):
+        os.makedirs(gold_labels_path)
+        print("Saving gold labels to: %s" % gold_labels_path)
+        np.savez_compressed(os.path.join(gold_labels_path, "gold_labels.npz"), labels=gold_labels)
     else:
         print("Vectors dump is already generated")
 
@@ -142,6 +172,13 @@ def generate_references():
                     assert np.allclose(first_labels, current_labels)
                     assert np.allclose(first_costs, current_costs)
                     assert np.allclose(first_score, current_score)
+    print("Verified equal result for marked tests")
+
+    for setup, ref_ami in ami_values.items():
+        labels, _, _ = load(setup)
+        ami = metrics.adjusted_mutual_info_score(gold_labels, labels)
+        assert np.isclose(ami, ref_ami)
+    print("Verified ami scores")
 
 
 @pytest.mark.parametrize("setup_name", list(setups.keys()))
