f38b83240affd1e083ca941427da35600ea3c700
Assaf Toledo <assaf.toledo@ibm.com>
2023-08-11 23:16:36 +0300

    Update setup.py - add Python 3.11

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Operating System :: Unix',
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',        
