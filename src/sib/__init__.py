4f0a5140f91b99c23bf6302f3a13420e7f3d44a9
Zachary Branson <Zac.Branson@ibm.com>
2025-04-14 11:24:17 -0400

    update cibuildwheel and merge changes from upstream
    Signed-off-by: Zachary Branson <Zac.Branson@ibm.com>

--- src/sib/__init__.py
+++ src/sib/__init__.py
@@ -16,6 +16,5 @@ import sib.c_package  # cython with cpp version
 from .sib_main import *
 
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
